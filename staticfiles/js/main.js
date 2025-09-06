fca64ff182f2fe69c298ec3475e75a73ba6b2052
Ilya Morozov <morozov1308@yandex.ru>
2025-09-06 08:45:08 +0300

    exam

--- staticfiles/js/main.js
+++ staticfiles/js/main.js
@@ -1,6 +1,179 @@
+$(document).ready(function() {
+	$('.footer_').fadeOut();
+	$('.footer__').fadeOut();
+	var hoverTimeout;
+	$('.part').hover(		
+		function() {	
+			var descriptionText = $(this).attr('description-data'); // Получаем текст
+			var imageUrl = $(this).attr('description-data-img'); // Получаем путь к изображению
+			$('.description').html('<p>' + descriptionText + '</p><img src="' + imageUrl + '" alt="Описание изображения">');
+					
+			
+
+			hoverTimeout = setTimeout(function() {
+				$('.description').fadeIn();
+			}, 300);
+		},
+		function() {
+			clearTimeout(hoverTimeout);
+			$('.description').fadeOut(50);	
+		},
+	)
+	base_Color = "#bf2523"
+	base_City_Color = "#006400"
+	color ="#8994f7"
+	
+
+	let scale_KFD_mass = [
+			{Level: '00', color: "#ca2323ff", mark: 10,},
+			{Level: '01', color: "#ec1008ff", mark: 20,},
+			{Level: '02', color: "#e13500", mark: 30,},
+			{Level: '03', color: "#eb5700", mark: 40,},
+			{Level: '04', color: "#f39708", mark: 50,},
+			{Level: '05', color: "#f2b10f", mark: 60,},
+			{Level: '06', color: "#f3d317", mark: 70,},
+			{Level: '07', color: "#c9dc26", mark: 80,},
+			{Level: '08', color: "#90bd31", mark: 90,},
+			{Level: '09', color: "#308a45", mark: 100,},		    
+		]
+
+	let scale_CHN_mass = [
+			
+			{Level: '00', color: "#f2b10f", mark: 20,},
+			{Level: '01', color: "#f3d317", mark: 40,},
+			{Level: '02', color: "#c9dc26", mark: 60,},
+			{Level: '03', color: "#90bd31", mark: 80,},
+			{Level: '04', color: "#308a45", mark: 100,},		    
+		]
+	
+	
+	// console.log(KFD_data_mass_prepare)
+
+	let KFD_data_mass = [];
+	let FG_data_mass = []; 
+	let CHN_data_mass = []; 
+
+	for (let i = 0; i < KFD_data_mass_prepare.length; i++) { 
+			KFD_data_mass[i] = {}; // Инициализация объекта для каждого элемента
+			KFD_data_mass[i].loc = KFD_data_mass_prepare[i].path_class;
+			KFD_data_mass[i].mark = KFD_data_mass_prepare[i].avg_F;
+			KFD_data_mass[i].color = "";
+					
+			FG_data_mass[i] = {}; // Инициализация объекта для каждого элемента
+			FG_data_mass[i].loc = KFD_data_mass_prepare[i].path_class;
+			FG_data_mass[i].mark = KFD_data_mass_prepare[i].avg_FG;
+			FG_data_mass[i].color = "";	
+
+			CHN_data_mass[i] = {}; // Инициализация объекта для каждого элемента
+			CHN_data_mass[i].loc = KFD_data_mass_prepare[i].path_class;
+			CHN_data_mass[i].mark = KFD_data_mass_prepare[i].avg_CHN;
+			CHN_data_mass[i].color = "";	
+		}  
+
+	let but_1 = document.querySelector('.but_1'),
+		but_2 = document.querySelector('.but_2'),
+		but_3 = document.querySelector('.but_3'),
+		but_4 = document.querySelector('.but_4'),
+		but_5 = document.querySelector('.but_5');
+	
+	but_1.onclick = function() {
+		$('.footer_').fadeOut(0);
+		$('.footer__').fadeOut(0);
+		var paths = document.querySelectorAll("path"), i;
+		for (i = 0; i < paths.length; i++) {			
+			paths[i].setAttribute('style', 'fill:'+base_Color);
+		}
+		var paths = document.querySelectorAll(".city"), i;
+		for (i = 0; i < paths.length; i++) {			
+			paths[i].setAttribute('style', 'fill:'+base_City_Color);
+		}
+	}
+	// color = '';
+	// mark_loc=0
+	
+	but_2.onclick = function() {
+		
+		$('.footer__').fadeOut(0);
+		$('.footer_').fadeOut(0);
+		for (i = 0; i < KFD_data_mass.length; i++) { 
+			for (j = 0; j < scale_KFD_mass.length; j++) {
+				if (scale_KFD_mass[j].mark - KFD_data_mass[i].mark >=0 && scale_KFD_mass[j].mark - KFD_data_mass[i].mark < 10) {
+					KFD_data_mass[i].color=scale_KFD_mass[j].color;
+				}
+			}
+		}		
+    	var paths = document.querySelectorAll("path"), i;
+		for (i = 0; i < paths.length; i++) {		
+			className = paths[i].getAttribute("class");			
+			for (j = 0; j < KFD_data_mass.length; j++) {				
+				if (className.includes(KFD_data_mass[j].loc)) {
+					mark_loc= KFD_data_mass[j].mark;					
+					paths[i].setAttribute('style', 'fill:'+KFD_data_mass[j].color);
+				}
+			}		    		
+		}
+		$('.footer_').fadeIn();	
+	}		
+	
+	but_3.onclick = function() {
+		
+		$('.footer__').fadeOut(0);
+		$('.footer_').fadeOut(0);
+		for (i = 0; i < FG_data_mass.length; i++) { 
+			for (j = 0; j < scale_KFD_mass.length; j++) {
+				if (scale_KFD_mass[j].mark - FG_data_mass[i].mark >=0 && scale_KFD_mass[j].mark - FG_data_mass[i].mark < 10) {
+					FG_data_mass[i].color=scale_KFD_mass[j].color;
+				}
+			}
+		}		
+    	var paths = document.querySelectorAll("path"), i;
+		for (i = 0; i < paths.length; i++) {		
+			className = paths[i].getAttribute("class");			
+			for (j = 0; j < FG_data_mass.length; j++) {				
+				if (className.includes(FG_data_mass[j].loc)) {
+					mark_loc= FG_data_mass[j].mark;					
+					paths[i].setAttribute('style', 'fill:'+FG_data_mass[j].color);
+				}
+			}		    		
+		}
+		$('.footer_').fadeIn();	
+	}
+
+	but_4.onclick = function() {
+		$('.footer_').fadeOut(0);
+		$('.footer__').fadeOut(0);
+		for (i = 0; i < CHN_data_mass.length; i++) { 
+			for (j = 0; j < scale_CHN_mass.length; j++) {
+				if (scale_CHN_mass[j].mark - CHN_data_mass[i].mark >=0 && scale_CHN_mass[j].mark - CHN_data_mass[i].mark < 20) {
+					CHN_data_mass[i].color = scale_CHN_mass[j].color;
+				}	
+				if (CHN_data_mass[i].mark >= 100) {
+					CHN_data_mass[i].color = scale_CHN_mass[4].color;
+					CHN_data_mass[i].color = "#09f13fff";
+				}
+			}
+		}		
+    	var paths = document.querySelectorAll("path"), i;
+		for (i = 0; i < paths.length; i++) {		
+			className = paths[i].getAttribute("class");			
+			for (j = 0; j < CHN_data_mass.length; j++) {				
+				if (className.includes(CHN_data_mass[j].loc)) {
+					mark_loc= CHN_data_mass[j].mark;					
+					paths[i].setAttribute('style', 'fill:'+CHN_data_mass[j].color);
+				}
+			}		    		
+		}
+		$('.footer__').fadeIn();	
+	}
+
+	but_5.onclick = function() {
+		window.location.href = "regionreport";
+			
+	}
+
+}) // $(document).ready(function()
+
 document.addEventListener('DOMContentLoaded', function() {
-    const masterSelect = document.querySelector('#id_master');
-    const servicesSelect = document.querySelector('#id_services');    
 
     // Функция для получения CSRF токена из cookie
     function getCookie(name) {
@@ -18,49 +191,10 @@ document.addEventListener('DOMContentLoaded', function() {
         return cookieValue;
     }
     const csrftoken = getCookie('csrftoken');
-    // if (masterSelect) {
-    //     alert('первый этап');
-    // }
-    // if (servicesSelect) {
-    //     alert('второй этап');
-    // }
 
 
-    if (masterSelect && servicesSelect) {
-        // alert('первый этап');
-        const updateServices = () => {
-            const masterId = masterSelect.value;
-            if (masterId) {
-                fetch('/ajax/get-master-services/', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'X-CSRFToken': csrftoken
-                    },
-                    body: JSON.stringify({ master_id: masterId })
-                })
-                .then(response => response.json())
-                .then(data => {
-                    servicesSelect.innerHTML = ''; // Очищаем список услуг
-                    if (data.services) {
-                        data.services.forEach(service => {
-                            const option = document.createElement('option');
-                            option.value = service.id;
-                            option.textContent = service.description;
-                            servicesSelect.appendChild(option);
-                        });
-                    }
-                })
-                .catch(error => console.error('Ошибка при загрузке услуг:', error));
-            } else {
-                servicesSelect.innerHTML = ''; // Очищаем, если мастер не выбран
-            }
-        };
 
-        masterSelect.addEventListener('change', updateServices);
-        // Вызываем функцию при загрузке страницы
-        updateServices();
-    }
+
     const toastElList = document.querySelectorAll(".toast");
     const toastList = [...toastElList].map((toastEl) => {
         const toast = new bootstrap.Toast(toastEl, {
@@ -70,4 +204,5 @@ document.addEventListener('DOMContentLoaded', function() {
         return toast;
     });
 });
+
 
